1236b16af616f813da87ca9d123228bd3814bca6
contemplator <contemplation8213@gmail.com>
2017-09-28 10:56:26 +0800

    增加 animation 模組

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -12,9 +13,9 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
-        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
